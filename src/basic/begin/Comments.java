965e4b83be0541cfe6119e3536a991c80c9daf0b
byungjun <bj8708@naver.com>
2025-02-21 12:29:00 +0900

    20250221 - 자바 시작 (변수,식별자)

---
+++ src/basic/begin/Comments.java
@@ -0,0 +1,16 @@
+package basic.begin;
+
+import java.sql.SQLOutput;
+
+public class Comments {
+    public static void main(String[] args) {
+
+        // 자바의 한 줄 주석
+
+        /*
+        System.out.println("Hello Java");
+        System.out.println("메롱메롱");
+*/
+        System.out.println("야호야호");
+    }
+}
