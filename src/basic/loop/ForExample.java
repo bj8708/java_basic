718034f00f787a85bee67679ae43a7599d005c2b
byungjun <bj8708@naver.com>
2025-02-25 10:50:30 +0900

    20250225 -반복문

---
+++ src/basic/loop/ForExample.java
@@ -0,0 +1,29 @@
+package basic.loop;
+
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+
+        /*
+         int j = 1; // begin
+        int total = 0; // 누적합을 기억시킬 변수
+        while (j <= 10) { // end
+            total += j;
+            j++; // step
+         */
+        int total = 0;
+        for (int j = 1; j <= 10; j++) ;
+
+
+    }}
+
+    // 1 ~ 200까지의 정수 중 6의 배수이면서 12의 배수는 아닌 수를 가로로 출력
+
+        // 정수를 하나 입력받아서
+// 입력받은 정수까지의 팩토리얼 값을 구하세요.
+// 팩토리얼) 5! -> 5 x 4 x 3 x 2 x 1
+
+
+
---
+++ src/basic/loop/WhileExample2.java
@@ -0,0 +1,42 @@
+package basic.loop;
+
+import java.sql.SQLOutput;
+
+public class WhileExample2 {
+
+    public static void main(String[] args) {
+
+        int q = 1;
+        int count = 0;{
+            while (q <= 1000);{
+            if (1000 % q == 0) {
+                count++;
+            }
+            q++;
+            }
+            System.out.println("count = " + count);
+        }}
+
+        // 48 ~ 150 사이의 정수 중 8의 배수만 가로로 출력해 보세요.
+
+
+
+
+
+        // 1 ~ 100까지의 정수 중 4의 배수이면서
+        // 8의 배수는 아닌 수를 가로로 출력해 보세요.
+         // 1 ~ 30000 까지의 정수 중 258의 배수의 개수를 출력.
+
+
+
+
+
+
+        }
+// 1000의 약수의 개수를 출력하세요.
+
+
+
+
+
+
